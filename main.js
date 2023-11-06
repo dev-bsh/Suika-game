47f9179091e47ead5bf8a1238afccfb38a45ea7f
BSH <gamaugi11@gmail.com>
2023-11-06 22:59:20 +0900

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+import { Engine, Render, Runner } from "matter-js";
+
+const engine = Engine.create();
+const render = Render.create({
+  engine,
+  element: document.body,
+  options: {
+    
+  }
+
+
+});
